db46fd940380d48c4215a947e68038907073d2c0
TylerAStocks <tylerastocks@gmail.com>
2025-09-10 12:59:40 -0400

    add GET and POST commands

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,27 +1,16 @@
 const mongoose = require('mongoose')
 
-
-
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
 
 
-const me = new User({
-    name: 'Tyler',
-    age: 26
-})
-
-
-me.save().then(() => {
+/* me.save().then(() => {
     console.log(me)
 }).catch((error) => {
     console.log(`Error: ${error}`)
-})
+}) */
+
+
+
+
+
