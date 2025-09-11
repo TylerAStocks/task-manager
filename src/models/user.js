75a675cd10feed041f098a562dd8e08be348ffa9
TylerAStocks <tylerastocks@gmail.com>
2025-09-11 15:42:26 -0400

    remove password and tokens from response body

--- src/models/user.js
+++ src/models/user.js
@@ -62,7 +62,18 @@ userSchema.methods.generateAuthToken = async function() {
 
 }
 
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
 
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+
+}
+
+// accessible on model
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 
